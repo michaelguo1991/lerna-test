418aef7b5d832fc3b4b850f7df3ebffcf9672d85
xufeng.guo <xufeng.guo@envision-digital.com>
2019-11-18 20:52:35 +0800

    lerna初始提交

---
+++ packages/my-lerna-first/index.js
@@ -0,0 +1,7 @@
+export function test() {
+  console.log('test');
+}
+
+export function print(str) {
+  console.log('print str: ', str);
+}
---
+++ packages/my-lerna-second/index.js
@@ -0,0 +1,3 @@
+import {print} from 'my-lerna-first';
+
+print('test');
